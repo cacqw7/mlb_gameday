de52364be25f5b80a3fcfad90f09281bc24ddcfb
Chase Clettenberg <chase.clettenberg@cph.org>
2016-03-13 17:09:46 -0500

    Move MockApi into a test helper

--- test/game_spec.rb
+++ test/game_spec.rb
@@ -1,28 +1,6 @@
 require 'minitest/autorun'
+require './test/test_helper.rb'
 require 'mlb_gameday.rb'
-
-# Mock the basic `open` function so we don't actually hit the MLB website
-class MockedApi < MLBGameday::API
-  alias_method :old_open, :open
-
-  def open(url, &block)
-    dir = File.dirname __FILE__
-    base = url.gsub 'http://gd2.mlb.com/components/game/mlb/', ''
-    path = File.join dir, base
-
-    unless File.exist?(path)
-      puts "Downloading from website: #{url}"
-
-      return old_open(url, &block)
-    end
-
-    file = File.open path
-
-    return file unless block_given?
-
-    block.call file
-  end
-end
 
 class TestGame < MiniTest::Test
   def setup
